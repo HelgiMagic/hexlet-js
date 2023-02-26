dc20e62b479462cc64151667063846f970482b47
Helgi <helgimagic@yandex.ru>
2023-02-26 11:38:38 +0300

    added a lot of files, .gitignore, lodash etc

---
+++ index.js
@@ -0,0 +1,9 @@
+import _ from "lodash";
+console.log(Math.sqrt(256) + 100);
+console.log('Hello, Hexlet!');
+console.log('123')
+function bebraLog() {
+    console.log('bebra');
+}
+export { bebraLog };
+console.log(_.last(['one', 'two']));
---
+++ testimimport.js
@@ -0,0 +1,1 @@
+import { bebraLog } from "./index.js";
